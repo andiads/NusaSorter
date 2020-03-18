f34e5325fb55f45064968557954ceeffedfa4205
Andi Dwi <andiads06@gmail.com>
2020-03-18 20:07:15 +0700

    change method properties

--- src/nusasort/Main.java
+++ src/nusasort/Main.java
@@ -22,36 +22,55 @@ public class Main {
         ArrayList<ModelNasabah> selAsc = new ArrayList<ModelNasabah>();
         ArrayList<ModelNasabah> reversed = new ArrayList<ModelNasabah>();
         
-        System.out.println("-------------------------------");
-         System.out.println("bubblesort asc by NAME");
-        System.out.println("+ID\t|\tNAME\t\t");
-        bubbleAsc = ns.doBubbleSort(true);
+        System.out.println("-----------------------------------------------");
+         System.out.println("bubblesort by Username");
+        System.out.println("+ID\t|\tNAME\t\t|\tNIK\t\t|\tUSERNAME\t\t");
+        
+        bubbleAsc = ns.doBubbleSort(ns.getDbList(), "username");
+        
         for (int i=0;i<bubbleAsc.size();i++){
-            System.out.println("+"+bubbleAsc.get(i).getId()+"\t|"+bubbleAsc.get(i).getNama());
+            System.out.println("+"+bubbleAsc.get(i).getId()
+                    +"\t|"+bubbleAsc.get(i).getNama()
+                    +"\t\t\t|"+bubbleAsc.get(i).getNik()
+                    +"\t\t|"+bubbleAsc.get(i).getUsername());
         }
         
-        System.out.println("-------------------------------");
-        System.out.println("Insertion sort asc by NAME");
-        System.out.println("+ID\t|\tNAME\t\t");
-        insAsc = ns.doInsertionSort();
+        System.out.println("-----------------------------------------------");
+        System.out.println("Insertion sort by NAME");
+        System.out.println("+ID\t|\tNAME\t\t|\tNIK\t\t|\tUSERNAME\t\t");
+        
+        insAsc = ns.doInsertionSort(ns.getDbList(), "name");
+        
         for (int i=0;i<insAsc.size();i++){
-            System.out.println("+"+insAsc.get(i).getId()+"\t|"+insAsc.get(i).getNama());
+           System.out.println("+"+insAsc.get(i).getId()
+                    +"\t|"+insAsc.get(i).getNama()
+                    +"\t\t\t|"+insAsc.get(i).getNik()
+                    +"\t\t|"+insAsc.get(i).getUsername());
         }
         
-        System.out.println("-------------------------------");
-        System.out.println("Selection sort asc by NAME");
-        System.out.println("+ID\t|\tNAME\t\t");
-        selAsc = ns.doSelectionSort();
+        System.out.println("-----------------------------------------------");
+        System.out.println("Selection sort by NIK");
+        System.out.println("+ID\t|\tNAME\t\t|\tNIK\t\t|\tUSERNAME\t\t");
+        
+        selAsc = ns.doSelectionSort(ns.getDbList(), "nik");
+        
         for (int i=0; i<selAsc.size(); i++) {
-            System.out.println("+"+selAsc.get(i).getId()+"\t|"+selAsc.get(i).getNama());
+            System.out.println("+"+insAsc.get(i).getId()
+                    +"\t|"+selAsc.get(i).getNama()
+                    +"\t\t\t|"+selAsc.get(i).getNik()
+                    +"\t\t|"+selAsc.get(i).getUsername());
         }
         
-        System.out.println("-------------------------------");
+        System.out.println("-----------------------------------------------");
         System.out.println("Reversed/Descending by ID");
         System.out.println("+ID\t|\tNAME\t\t");
         reversed = ns.doReverse();
         for (int i=0; i<selAsc.size(); i++) {
-            System.out.println("+"+reversed.get(i).getId()+"\t|"+reversed.get(i).getNama());
+            System.out.println("+"+reversed.get(i).getId()
+                    +"\t|"+reversed.get(i).getNama()
+                    +"\t\t\t|"+reversed.get(i).getNik()
+                    +"\t\t|"+reversed.get(i).getUsername());
         }
+        System.out.println("-----------------------------------------------");
     }
 }
--- src/nusasort/NusaSorter.java
+++ src/nusasort/NusaSorter.java
@@ -5,6 +5,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.lang.reflect.Field;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import nusasort.ModelNasabah;
 import nusasort.DBConnection;
 
@@ -20,13 +24,21 @@ import nusasort.DBConnection;
 public class NusaSorter {
 
     private ArrayList<ModelNasabah> listNasabah;
+    Class nasabahClass;
+    private Field id;
+    private Field name;
+    private Field nik;
+    private Field username;
+    private Field field;
     private boolean isSorted;
     private ModelNasabah nsb;
+    private String key;
 
     public NusaSorter() {
 
     }
 
+    // INITIALIZE TABLE NASABAH 
     public ArrayList<ModelNasabah> getDbList() {
         listNasabah = new ArrayList<ModelNasabah>();
         try {
@@ -37,6 +49,8 @@ public class NusaSorter {
                 nsb = new ModelNasabah();
                 nsb.setId(rs.getInt("id_nasabah"));
                 nsb.setNama(rs.getString("nama"));
+                nsb.setNik(rs.getInt("nik"));
+                nsb.setUsername(rs.getString("username_nasabah"));
                 listNasabah.add(nsb);
             }
 
@@ -48,21 +62,41 @@ public class NusaSorter {
         return listNasabah;
     }
 
-    public ArrayList<ModelNasabah> doBubbleSort(boolean ascending) {
-
-        listNasabah = getDbList();
+    // BUBBLE SORT
+    public ArrayList<ModelNasabah> doBubbleSort(ArrayList<ModelNasabah> obj, String key) {
+        listNasabah = obj;
 
         ModelNasabah temp = null;
-        // algo
+
         if (listNasabah.size() > 1) {
             for (int x = 0; x < listNasabah.size(); x++) {
                 isSorted = true;
                 for (int i = 0; i < (listNasabah.size() - x - 1); i++) {
 
-                    if ((listNasabah.get(i).getNama()).compareToIgnoreCase(listNasabah.get(i+1).getNama()) > 0) {
-                        temp = (listNasabah.get(i));
-                        listNasabah.set(i, listNasabah.get(i + 1));
-                        listNasabah.set(i + 1, temp);
+                    if (key.equalsIgnoreCase("id")) {
+                        if ((listNasabah.get(i).getId()) > (listNasabah.get(i + 1).getId())) {
+                            temp = (listNasabah.get(i));
+                            listNasabah.set(i, listNasabah.get(i + 1));
+                            listNasabah.set(i + 1, temp);
+                        }
+                    } else if (key.equalsIgnoreCase("name")) {
+                        if ((listNasabah.get(i).getNama()).compareToIgnoreCase(listNasabah.get(i + 1).getNama()) > 0) {
+                            temp = (listNasabah.get(i));
+                            listNasabah.set(i, listNasabah.get(i + 1));
+                            listNasabah.set(i + 1, temp);
+                        }
+                    } else if (key.equalsIgnoreCase("nik")) {
+                        if ((listNasabah.get(i).getNik()) > (listNasabah.get(i + 1).getNik())) {
+                            temp = (listNasabah.get(i));
+                            listNasabah.set(i, listNasabah.get(i + 1));
+                            listNasabah.set(i + 1, temp);
+                        }
+                    } else if (key.equalsIgnoreCase("username")) {
+                        if ((listNasabah.get(i).getUsername()).compareToIgnoreCase(listNasabah.get(i + 1).getUsername()) > 0) {
+                            temp = (listNasabah.get(i));
+                            listNasabah.set(i, listNasabah.get(i + 1));
+                            listNasabah.set(i + 1, temp);
+                        }
                     }
 
                 }
@@ -73,72 +107,163 @@ public class NusaSorter {
         return listNasabah;
     }
 
-    public ArrayList<ModelNasabah> doSelectionSort() {
+    // SELECTION SORT
+    public ArrayList<ModelNasabah> doSelectionSort(ArrayList<ModelNasabah> obj, String key) {
         ModelNasabah nasabah = new ModelNasabah();
-        listNasabah = getDbList();
-        for (int i=0; i<listNasabah.size();i++){
-            int min = listNasabah.get(i).getId();
-            int minIndex = 1;
-            
-            for (int j = i+1; j < listNasabah.size(); j++) {
-                if (listNasabah.get(j).getId() < min) {
-                    min = listNasabah.get(j).getId();
+        listNasabah = obj;
+        
+        if (key.equalsIgnoreCase("id")){
+            for (int i = 0; i < listNasabah.size(); i++) {
+                int min = listNasabah.get(i).getId();
+                int minIndex = 1;
+
+                for (int j = i + 1; j < listNasabah.size(); j++) {
+                    if (listNasabah.get(j).getId() < min) {
+                        min = listNasabah.get(j).getId();
+                    }
+                }
+                if (i != minIndex) {
+                    Collections.swap(listNasabah, i, minIndex);
                 }
             }
-            if (i != minIndex) {
-                Collections.swap(listNasabah, i, minIndex);
+        } else if (key.equalsIgnoreCase("name")) {
+            for (int i = 0; i < listNasabah.size(); i++) {
+                String tempName = listNasabah.get(i).getNama();
+                int minIndex = 1;
+
+                for (int j = i + 1; j < listNasabah.size(); j++) {
+                    if (listNasabah.get(j).getNama().compareToIgnoreCase(tempName)>0) {
+                        tempName = listNasabah.get(j).getNama();
+                    }
+                }
+                if (i != minIndex) {
+                    Collections.swap(listNasabah, i, minIndex);
+                }
+            }
+        } else if (key.equalsIgnoreCase("nik")) {
+            for (int i = 0; i < listNasabah.size(); i++) {
+                int tempNik = listNasabah.get(i).getNik();
+                int minIndex = 1;
+
+                for (int j = i + 1; j < listNasabah.size(); j++) {
+                    if (listNasabah.get(j).getId() < tempNik) {
+                        tempNik = listNasabah.get(j).getId();
+                    }
+                }
+                if (i != minIndex) {
+                    Collections.swap(listNasabah, i, minIndex);
+                }
+            }
+        } else if (key.equalsIgnoreCase("username")) {
+            for (int i = 0; i < listNasabah.size(); i++) {
+                String tempUsername = listNasabah.get(i).getUsername();
+                int minIndex = 1;
+
+                for (int j = i + 1; j < listNasabah.size(); j++) {
+                    if (listNasabah.get(j).getUsername().compareToIgnoreCase(tempUsername)>0) {
+                        tempUsername = listNasabah.get(j).getUsername();
+                    }
+                }
+                if (i != minIndex) {
+                    Collections.swap(listNasabah, i, minIndex);
+                }
             }
         }
+        
+        
         return listNasabah;
     }
 
-    public ArrayList<ModelNasabah> doInsertionSort() {
-        
+    // INSERTION SORT
+    public ArrayList<ModelNasabah> doInsertionSort(ArrayList<ModelNasabah> obj, String key) {
+
         listNasabah = getDbList();
-        int i,j;
-        
-        /* with int id
-        for (i = 1; i < listNasabah.size(); i++) {
-            ModelNasabah current = new ModelNasabah();
-            current.setNama(listNasabah.get(i).getNama());
-            current.setId(listNasabah.get(i).getId());
-            j = i;
-            while ((j > 0) && (listNasabah.get(j - 1).getId() > current.getId())) {
-                listNasabah.set(i, listNasabah.get(j - 1));
-                i--;
+        ModelNasabah current;
+        int i, j;
+
+        if (key.equalsIgnoreCase("id")) {
+            // with int id
+            for (i = 1; i < listNasabah.size(); i++) {
+                current = new ModelNasabah();
+                current.setNama(listNasabah.get(i).getNama());
+                current.setId(listNasabah.get(i).getId());
+                current.setNik(listNasabah.get(i).getNik());
+                current.setUsername(listNasabah.get(i).getUsername());
+                j = i;
+                while ((j > 0) && (listNasabah.get(j - 1).getId() > current.getId())) {
+                    listNasabah.set(i, listNasabah.get(j - 1));
+                    i--;
+                }
+                listNasabah.set(j, current);
             }
-            listNasabah.set(j, current);
-        }
-        */
-        
-        // with string nama
-        for (i = 1; i < listNasabah.size(); i++) {
-            ModelNasabah current = new ModelNasabah();
-            String curName;
-            current.setNama(listNasabah.get(i).getNama());
-            current.setId(listNasabah.get(i).getId());
-            curName = current.getNama();
-            j = i - 1;
-            while ((j >= 0) && (curName.compareToIgnoreCase(listNasabah.get(j).getNama()) < 0)) {
-                listNasabah.set(j+1, listNasabah.get(j));
-                j--;
+
+        } else if (key.equalsIgnoreCase("name")) {
+            // with string nama
+            for (i = 1; i < listNasabah.size(); i++) {
+                String curName;
+                current = new ModelNasabah();
+                current.setNama(listNasabah.get(i).getNama());
+                current.setId(listNasabah.get(i).getId());
+                current.setNik(listNasabah.get(i).getNik());
+                current.setUsername(listNasabah.get(i).getUsername());
+                curName = current.getNama();
+                j = i - 1;
+                while ((j >= 0) && (curName.compareToIgnoreCase(listNasabah.get(j).getNama()) < 0)) {
+                    listNasabah.set(j + 1, listNasabah.get(j));
+                    j--;
+                }
+                listNasabah.set(j + 1, current);
             }
-            listNasabah.set(j+1, current);
+        } else if (key.equalsIgnoreCase("nik")) {
+            // with int nik
+            for (i = 1; i < listNasabah.size(); i++) {
+                current = new ModelNasabah();
+                current.setNama(listNasabah.get(i).getNama());
+                current.setId(listNasabah.get(i).getId());
+                current.setNik(listNasabah.get(i).getNik());
+                current.setUsername(listNasabah.get(i).getUsername());
+                j = i;
+                while ((j > 0) && (listNasabah.get(j - 1).getNik() > current.getNik())) {
+                    listNasabah.set(i, listNasabah.get(j - 1));
+                    i--;
+                }
+                listNasabah.set(j, current);
+            }
+        } else if (key.equalsIgnoreCase("username")) {
+            // with string username
+            for (i = 1; i < listNasabah.size(); i++) {
+                current = new ModelNasabah();
+                String curUsername;
+                current.setNama(listNasabah.get(i).getNama());
+                current.setId(listNasabah.get(i).getId());
+                current.setNik(listNasabah.get(i).getNik());
+                current.setUsername(listNasabah.get(i).getUsername());
+                curUsername = current.getUsername();
+
+                j = i - 1;
+                while ((j >= 0) && (curUsername.compareToIgnoreCase(listNasabah.get(j).getUsername()) < 0)) {
+                    listNasabah.set(j + 1, listNasabah.get(j));
+                    j--;
+                }
+                listNasabah.set(j + 1, current);
+            }
+
         }
-        
-        
+
         return listNasabah;
     }
 
+    // MERGE SORT
     public void doMergeSort() {
-        
+
     }
-    
+
+    // REVERSE
     public ArrayList<ModelNasabah> doReverse() {
         listNasabah = getDbList();
         ArrayList<ModelNasabah> reversedList = new ArrayList<ModelNasabah>();
         // store arraylist as  reversed elements
-        for (int i = listNasabah.size() - 1; i>=0; i-- ) {
+        for (int i = listNasabah.size() - 1; i >= 0; i--) {
             //append the elements in reverse order
             reversedList.add(listNasabah.get(i));
         }
