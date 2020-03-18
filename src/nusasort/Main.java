c64a3c52157d52d90c7d067173a1e802d53607f4
Andi Dwi <andiads06@gmail.com>
2020-03-18 20:46:03 +0700

    naming convention

--- src/nusasort/Main.java
+++ src/nusasort/Main.java
@@ -17,55 +17,55 @@ import nusasort.NusaSorter;
 public class Main {
     public static void main(String[]args){
         NusaSorter ns = new NusaSorter();
-        ArrayList<ModelNasabah> bubbleAsc = new ArrayList<ModelNasabah>();
-        ArrayList<ModelNasabah> insAsc = new ArrayList<ModelNasabah>();
-        ArrayList<ModelNasabah> selAsc = new ArrayList<ModelNasabah>();
+        ArrayList<ModelNasabah> bubbleSort = new ArrayList<ModelNasabah>();
+        ArrayList<ModelNasabah> insertSort = new ArrayList<ModelNasabah>();
+        ArrayList<ModelNasabah> selectionSort = new ArrayList<ModelNasabah>();
         ArrayList<ModelNasabah> reversed = new ArrayList<ModelNasabah>();
         
         System.out.println("-----------------------------------------------");
          System.out.println("bubblesort by Username");
         System.out.println("+ID\t|\tNAME\t\t|\tNIK\t\t|\tUSERNAME\t\t");
         
-        bubbleAsc = ns.doBubbleSort(ns.getDbList(), "username");
+        bubbleSort = ns.doBubbleSort(ns.getDbList(), "username");
         
-        for (int i=0;i<bubbleAsc.size();i++){
-            System.out.println("+"+bubbleAsc.get(i).getId()
-                    +"\t|"+bubbleAsc.get(i).getNama()
-                    +"\t\t\t|"+bubbleAsc.get(i).getNik()
-                    +"\t\t|"+bubbleAsc.get(i).getUsername());
+        for (int i=0;i<bubbleSort.size();i++){
+            System.out.println("+"+bubbleSort.get(i).getId()
+                    +"\t|"+bubbleSort.get(i).getNama()
+                    +"\t\t\t|"+bubbleSort.get(i).getNik()
+                    +"\t\t|"+bubbleSort.get(i).getUsername());
         }
         
         System.out.println("-----------------------------------------------");
         System.out.println("Insertion sort by NAME");
         System.out.println("+ID\t|\tNAME\t\t|\tNIK\t\t|\tUSERNAME\t\t");
         
-        insAsc = ns.doInsertionSort(ns.getDbList(), "name");
+        insertSort = ns.doInsertionSort(ns.getDbList(), "name");
         
-        for (int i=0;i<insAsc.size();i++){
-           System.out.println("+"+insAsc.get(i).getId()
-                    +"\t|"+insAsc.get(i).getNama()
-                    +"\t\t\t|"+insAsc.get(i).getNik()
-                    +"\t\t|"+insAsc.get(i).getUsername());
+        for (int i=0;i<insertSort.size();i++){
+           System.out.println("+"+insertSort.get(i).getId()
+                    +"\t|"+insertSort.get(i).getNama()
+                    +"\t\t\t|"+insertSort.get(i).getNik()
+                    +"\t\t|"+insertSort.get(i).getUsername());
         }
         
         System.out.println("-----------------------------------------------");
         System.out.println("Selection sort by NIK");
         System.out.println("+ID\t|\tNAME\t\t|\tNIK\t\t|\tUSERNAME\t\t");
         
-        selAsc = ns.doSelectionSort(ns.getDbList(), "nik");
+        selectionSort = ns.doSelectionSort(ns.getDbList(), "nik");
         
-        for (int i=0; i<selAsc.size(); i++) {
-            System.out.println("+"+insAsc.get(i).getId()
-                    +"\t|"+selAsc.get(i).getNama()
-                    +"\t\t\t|"+selAsc.get(i).getNik()
-                    +"\t\t|"+selAsc.get(i).getUsername());
+        for (int i=0; i<selectionSort.size(); i++) {
+            System.out.println("+"+insertSort.get(i).getId()
+                    +"\t|"+selectionSort.get(i).getNama()
+                    +"\t\t\t|"+selectionSort.get(i).getNik()
+                    +"\t\t|"+selectionSort.get(i).getUsername());
         }
         
         System.out.println("-----------------------------------------------");
         System.out.println("Reversed/Descending by ID");
         System.out.println("+ID\t|\tNAME\t\t");
         reversed = ns.doReverse();
-        for (int i=0; i<selAsc.size(); i++) {
+        for (int i=0; i<selectionSort.size(); i++) {
             System.out.println("+"+reversed.get(i).getId()
                     +"\t|"+reversed.get(i).getNama()
                     +"\t\t\t|"+reversed.get(i).getNik()
